e074c685038d5175a9725b181a5d937b59d2cee8
AteCastillo <atecast@gmail.com>
2022-02-07 19:53:06 -0300

    task2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Write a function which takes a float n as argument and returns the floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    a function to concat strings
+    """
+    return int(n)
--- 0x00-python_variable_annotations/2-main.py
+++ 0x00-python_variable_annotations/2-main.py
@@ -7,5 +7,5 @@ floor = __import__('2-floor').floor
 ans = floor(3.14)
 
 print(ans == math.floor(3.14))
-print(floor.__annotations__o)
+print(floor.__annotations__)
 print("floor(3.14) returns {}, which is a {}".format(ans, type(ans)))
