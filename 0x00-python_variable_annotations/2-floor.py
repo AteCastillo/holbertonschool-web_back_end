00cec769e8010f13411507639b2ee0d2310b798f
AteCastillo <atecast@gmail.com>
2022-02-07 19:59:53 -0300

    adding new task

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -6,6 +6,6 @@ Write a function which takes a float as argument and returns the floor
 
 def floor(n: float) -> int:
     """
-    a function to concat strings
+    a function to return a floor
     """
     return int(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Write a function which takes a float and returns the string representation
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    a function to return a string
+    """
+    return str(n)
