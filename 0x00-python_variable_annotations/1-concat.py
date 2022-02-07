3b3adbe3bad6e864a2105293e19f636d71d2209f
AteCastillo <atecast@gmail.com>
2022-02-07 19:28:50 -0300

    task 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function to concat 2 strings
+"""
+
+def concat(a: str, b: str) -> str:
+    """
+    a function to concat strings
+    """
+    return (a + b)
