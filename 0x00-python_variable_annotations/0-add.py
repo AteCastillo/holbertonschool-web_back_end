e90c8f194081bee8b57b127366b74a16d6dd29af
AteCastillo <atecast@gmail.com>
2022-02-07 18:25:29 -0300

    fix

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,7 +3,7 @@
 Write a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float
 '''
 
-def add(a:float, b:float) -> float:
+def add(a: float, b: float) -> float:
     '''
     a function to sum floats
     '''
