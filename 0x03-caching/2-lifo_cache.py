856e18be35b3588e9f732fefe00a5b75c545bfcc
AteCastillo <atecast@gmail.com>
2022-02-21 17:59:02 -0300

    fix

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
 """
-Caching
+Create a class LIFOCache that inherits from BaseCaching
 """
 
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ 
-    Create a class LIFOCache that inherits from BaseCaching and is a caching system:
+    """
+    Create a class LIFOCache that inherits from BaseCaching
+    and is a caching system:
 
-    You must use self.cache_data - dictionary from the parent class BaseCaching
-    You can overload def __init__(self): but dont forget to call the parent init: super().__init__()
+    You must use self.cache_data - dictionary from the parent
+    class BaseCaching
+    You can overload def __init__(self): but dont forget to call
+    the parent init: super().__init__()
     def put(self, key, item):
-        Must assign to the dictionary self.cache_data the item value for the key key.
+        Must assign to the dictionary self.cache_data the item value
+        for the key key.
         If key or item is None, this method should not do anything.
-        If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
+        If the number of items in self.cache_data is higher that
+        BaseCaching.MAX_ITEMS:
             you must discard the last item put in cache (LIFO algorithm)
-            you must print DISCARD: with the key discarded and following by a new line
+            you must print DISCARD: with the key discarded and following
+            by a new line
     def get(self, key):
         Must return the value in self.cache_data linked to key.
-        If key is None or if the key doesnt exist in self.cache_data, return None.
+        If key is None or if the key doesnt exist in self.cache_data,
+        return None.
     """
     def __init__(self):
         super().__init__()
